d79f25961f135a60511ecf8b1954422472e19cdd
IvandryPage <ivandry.89@gmail.com>
2024-11-27 06:51:26 +0700

    Add fourteenth program for boolean operators

---
+++ 14th_program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "======== Comparing Operator ========" << std::endl;
+    int num1 {1}, num2 {2};
+    std::cout << "Number 1 : " << num1 << std::endl;
+    std::cout << "Number 2 : " << num2 << std::endl;
+
+    std::cout << '\n';
+    std::cout << std::boolalpha;
+    std::cout << "number 1 < number 2 : " << (num1 < num2) << std::endl;
+    std::cout << "number 1 <= number 2 : " << (num1 <= num2) << std::endl;
+    std::cout << "number 1 > number 2 : " << (num1 > num2) << std::endl;
+    std::cout << "number 1 >= number 2 : " << (num1 >= num2) << std::endl;
+    std::cout << "number 1 == number 2 : " << (num1 == num2) << std::endl;
+    std::cout << "number 1 != number 2 : " << (num1 != num2) << std::endl;
+    return 0;
+}
