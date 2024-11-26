2085acb67ae994884306cf52d1405868664cc96c
IvandryPage <ivandry.89@gmail.com>
2024-11-27 02:57:19 +0700

    Add fifth program for input output

--- 4th_program.cpp
+++ 4th_program.cpp
@@ -16,6 +16,8 @@ int main()
     int second_number = 9;
 
     // different way to assign value to int
+    // the benefit is it would cause an error if the "narrowing" or "losing the accuracy of an numeric value"
+    // for example : if I put 3.14 in the curly braces, it would cause an error
     int third_number {20};
     
     int sum = addNumbers(first_number, second_number);
