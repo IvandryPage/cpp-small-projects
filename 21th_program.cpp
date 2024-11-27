2e58d486cb7379bdafaeb30187626cfe541b0c23
IvandryPage <ivandry.89@gmail.com>
2024-11-27 09:51:26 +0700

    Add twenty first program for arrays

---
+++ 21th_program.cpp
@@ -0,0 +1,18 @@
+// array
+#include <iostream>
+
+int main()
+{
+    int scores[10] {12, 5, 10, 20, 30, 40, 90, 100, 55, 10}; // har garbage value
+
+    std::cout << "Data of the array: " << std::endl;
+    for (auto &score : scores)
+        std::cout << "score : " << score << '\n';
+
+    std::cout << "===================" << std::endl;
+    for(size_t i{0}; i < std::size(scores); i++)
+        std::cout << "score[" << i << "] :" << scores[i] << std::endl;
+
+    char nama[9] {"Ivandry"};
+    std::cout << nama << std::endl; // not safe if character array is not null terminated
+}
